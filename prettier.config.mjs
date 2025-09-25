32d0043d2c58681864f8833baf70770b95650440
Anonymous-Humanoid <68610963+Anonymous-Humanoid@users.noreply.github.com>
2025-09-25 16:53:58 -0400

    Migrated to Prettier JS config
    Signed-off-by: Anonymous-Humanoid <68610963+Anonymous-Humanoid@users.noreply.github.com>

---
+++ prettier.config.mjs
@@ -0,0 +1,32 @@
+/**
+ * The Prettier configuration
+ * @type {import("prettier").Config}
+ * @link https://prettier.io/docs/configuration
+ */
+const CONFIG = {
+    singleQuote: true,
+    trailingComma: "none",
+    requirePragma: false,
+    arrowParens: "always",
+    useTabs: false,
+    tabWidth: 4,
+    semi: true,
+    endOfLine: "lf",
+    printWidth: 80,
+    jsxSingleQuote: true,
+    singleAttributePerLine: false,
+    overrides: [
+        {
+            files: [
+                "**/*.md",
+                "**/*.yml",
+                "**/*.yaml"
+            ],
+            options: {
+                tabWidth: 2
+            }
+        }
+    ]
+}
+
+export default CONFIG;
